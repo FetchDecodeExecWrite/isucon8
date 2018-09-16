aa3d6f14f7f8357aab6aa759840126088d800fdf
moratorium08 <moratorium08@gmail.com>
2018-09-16 14:53:59 +0900

    Revert "Merge commit '01f9c0de783f1dceb8be1c7332693fb3372898d7'"
    This reverts commit ce70ec7e0636df86a113edd99596053e40396834, reversing
    changes made to ba13092b0482e50a116bad71ce9da0689dc62e1b.

--- torb/webapp/go/src/torb/app.go
+++ torb/webapp/go/src/torb/app.go
@@ -918,7 +918,7 @@ func reportSales(c echo.Context) error {
 }
 
 func reportSaleses(c echo.Context) error {
-	rows, err := db.Query("select r.*, s.rank as sheet_rank, s.num as sheet_num, s.price as sheet_price, r.event_id, r.event_price as event_price from reservations r inner join sheets s on s.id = r.sheet_id order by reserved_at asc")
+	rows, err := db.Query("select r.*, s.rank as sheet_rank, s.num as sheet_num, s.price as sheet_price, r.event_id, r.event_price as event_price from reservations r inner join sheets s on s.id = r.sheet_id by reserved_at asc")
 	if err != nil {
 		return err
 	}
@@ -995,10 +995,8 @@ func main() {
 	e.GET("/admin/api/reports/events/:id/sales", reportSales, adminLoginRequired)
 	e.GET("/admin/api/reports/sales", reportSaleses, adminLoginRequired)
 
-	if os.Getenv("DEBUG_ISUCON") == "" {
+	if true {
 		echopprof.Wrap(e)
-	} else {
-		fmt.Println("debugging...")
 	}
 
 	e.Start(":8080")
