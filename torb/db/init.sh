25c8340fe8a639182142eb0476f27536019a4678
cookie-s <cookie-s@users.noreply.github.com>
2018-09-19 02:02:26 +0900

    add index

--- torb/db/init.sh
+++ torb/db/init.sh
@@ -16,7 +16,10 @@ fi
 
 gzip -dc "$DB_DIR/isucon8q-initial-dataset.sql.gz" | mysql -uisucon torb
 
+mysql -uroot torb -e "set global max_connections = 10000"&
 mysql -uisucon torb -e "ALTER TABLE sheets ADD UNIQUE rank_num_uniq (\`rank\`, num)"&
+mysql -uisucon torb -e "ALTER TABLE reservations ADD INDEX reserve (reserved_at)"&
+mysql -uisucon torb -e "ALTER TABLE reservations ADD INDEX canceled (canceled_at)"&
 mysql -uisucon torb -e "ALTER TABLE reservations ADD INDEX user_id (user_id)"&
 mysql -uisucon torb -e "ALTER TABLE reservations ADD INDEX event_cancel (event_id, canceled_at)"&
 mysql -uisucon torb -e "ALTER TABLE reservations ADD UNIQUE uniq3 (event_id, sheet_id, canceled_at)"&
