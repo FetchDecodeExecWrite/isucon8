f34655377ebd5ce99092ab605c212906670f8aa8
cookie-s <cookie-s@users.noreply.github.com>
2018-09-19 04:36:44 +0900

    transaction

--- torb/webapp/go/src/torb/app.go
+++ torb/webapp/go/src/torb/app.go
@@ -751,7 +751,12 @@ func postReserve(c echo.Context) error {
 			return err
 		}
 
-		tx.Exec("SELECT id FROM event_id = ? AND sheet_id = ? AND canceled_at = '0000-00-00 00:00:00' FOR UPDATE", event.ID, sheet.ID)
+		if err := tx.Exec("SELECT id FROM event_id = ? AND sheet_id = ? AND canceled_at = '0000-00-00 00:00:00' FOR UPDATE", event.ID, sheet.ID); err != nil {
+			if err != sql.ErrNoRows {
+				tx.Rollback()
+				continue
+			}
+		}
 
 		res, err := tx.Exec("INSERT INTO reservations (event_id, sheet_id, user_id, reserved_at, event_price) VALUES (?, ?, ?, ?, ?)", event.ID, sheet.ID, user.ID, time.Now().UTC().Format("2006-01-02 15:04:05.000000"), event.Price)
 		if err != nil {
